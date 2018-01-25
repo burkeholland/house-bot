e04ef0d7befc6f9e6d9011f0ecb4d652a960a2bc
Burke Holland <burkeholland@Burkes-MacBook-Pro.local>
2018-01-25 15:13:05 -0600

    A little bit smarter now 🧠

--- message/index.js
+++ message/index.js
@@ -11,71 +11,127 @@ var connector = new botbuilder_azure.BotServiceConnector({
   openIdMetadata: process.env['BotOpenIdMetadata']
 });
 
-var bot = new builder.UniversalBot(connector, session => {
-  session.send(`Sorry, I did not understand "${session.message.text}"`);
-});
+var bot = new builder.UniversalBot(connector);
 
-var recognizer = new builder.LuisRecognizer(
-  'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/160b0864-1ba8-4aee-a337-b8c77695fc9f?subscription-key=19d29a12d3fc4d9084146b466638e62a&verbose=true&timezoneOffset=0&q='
-);
-bot.recognizer(recognizer);
+// var recognizer = new builder.LuisRecognizer(
+//   ''
+// );
+// bot.recognizer(recognizer);
 
-bot
-  .dialog('ControlLights', [
-    function(session, args, next) {
-      // session.send('OK! One sec...');
+// bot
+//   .dialog('ControlLights', [
+//     function(session, args, next) {
+//       // session.send('OK! One sec...');
 
-      var location = builder.EntityRecognizer.findEntity(
-        args.intent.entities,
-        'Location'
-      );
+//       var location = builder.EntityRecognizer.findEntity(
+//         args.intent.entities,
+//         'Location'
+//       );
 
-      var lightState = builder.EntityRecognizer.findEntity(
-        args.intent.entities,
-        'Light State'
-      );
+//       var lightState = builder.EntityRecognizer.findEntity(
+//         args.intent.entities,
+//         'Light State'
+//       );
 
-      // got both location and light state, move on to the next step
-      if (location && lightState) {
-        session.dialogData.location = location.entity;
-        next({
-          location: location.entity,
-          lightState: lightState.entity
-        });
-      }
+//       // got both location and light state, move on to the next step
+//       if (location && lightState) {
+//         session.dialogData.location = location.entity;
+//         next({
+//           location: location.entity,
+//           lightState: lightState.entity
+//         });
+//       }
 
-      // got a location, but no light state
-      if (!location || !lightState) {
-        session.dialogData.location = location;
-        builder.Prompts.text(
-          session,
-          `I need to know which room and if you want the lights on or off. You can say things like, "Turn on/off the kitchen lights".`
-        );
-      }
-    },
-    function(session, results) {
-      var location = results.location;
-      var lightState = results.lightState;
-      controlLights(session, location, lightState);
-    }
-  ])
-  .triggerAction({
-    matches: 'Control Lights'
-  });
+//       // got a location, but no light state
+//       if (!location || !lightState) {
+//         session.dialogData.location = location;
+//         builder.Prompts.text(
+//           session,
+//           `I need to know which room and if you want the lights on or off. You can say things like, "Turn on/off the kitchen lights".`
+//         );
+//       }
+//     },
+//     function(session, results) {
+//       var location = results.location;
+//       var lightState = results.lightState;
+//       controlLights(session, location, lightState);
+//     }
+//   ])
+//   .triggerAction({
+//     matches: 'Control Lights'
+//   });
 
-bot
-  .dialog('Help', function(session) {
-    session.endDialog(
-      `I can turn lights on or off in any room. You can say things like, "Turn off the lights in the kitchen".`
+// bot
+//   .dialog('Help', session => {
+//     session.endDialog(
+//       `I can turn lights on or off in any room. You can say things like, "Turn off the lights in the kitchen".`
+//     );
+//   })
+//   .triggerAction({
+//     matches: 'Help'
+//   });
+
+// bot
+//   .dialog('Greeting', session => {
+//     session.endDialog('Sup, yo!');
+//   })
+//   .triggerAction({
+//     matches: 'Greeting'
+//   });
+
+// Main dialog with LUIS
+const recognizer = new builder.LuisRecognizer(process.env['LuisModelUrl']);
+const intents = new builder.IntentDialog({ recognizers: [recognizer] })
+  .matches('Greeting', session => {
+    session.send('Sup, yo!');
+  })
+  .matches('Help', session => {
+    session.send(
+      'I can control the lights in your house. You can say things like, "Turn the kitchen lights on".'
     );
   })
-  .triggerAction({
-    matches: 'Help'
+  .matches('Cancel', session => {
+    session.send('OK. Canceled.');
+    session.endDialog();
+  })
+  .matches('Control Lights', (session, args) => {
+    session.send('OK! One sec...');
+
+    var location = builder.EntityRecognizer.findEntity(
+      args.entities,
+      'Location'
+    );
+
+    var lightState = builder.EntityRecognizer.findEntity(
+      args.entities,
+      'Light State'
+    );
+
+    // got both location and light state, move on to the next step
+    if (location && lightState) {
+      controlLights(session, location.entity, lightState.entity);
+    }
+
+    // got a location, but no light state
+    if (!location || !lightState) {
+      session.dialogData.location = location;
+      builder.Prompts.text(
+        session,
+        `I need to know which room and if you want the lights on or off. You can say things like, "Turn on/off the kitchen lights".`
+      );
+    }
+  })
+  /*
+.matches('<yourIntent>')... See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/
+*/
+  .onDefault(session => {
+    session.send("Sorry, I did not understand '%s'.", session.message.text);
   });
+
+bot.dialog('/', intents);
 
 function controlLights(session, location, lightState) {
   session.send(`${location} lights are ${lightState}`);
-
   session.endDialog();
 }
 
