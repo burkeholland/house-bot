7ed841e903d325e820db989344fa8199faa5e17f
Burke Holland <burkeholland@Burkes-MacBook-Pro.local>
2018-01-25 18:12:25 -0600

    Change prompts to send. No dialog waterfall

--- messages/index.js
+++ messages/index.js
@@ -13,72 +13,6 @@ var connector = new botbuilder_azure.BotServiceConnector({
 
 var bot = new builder.UniversalBot(connector);
 
-// var recognizer = new builder.LuisRecognizer(
-//   ''
-// );
-// bot.recognizer(recognizer);
-
-// bot
-//   .dialog('ControlLights', [
-//     function(session, args, next) {
-//       // session.send('OK! One sec...');
-
-//       var location = builder.EntityRecognizer.findEntity(
-//         args.intent.entities,
-//         'Location'
-//       );
-
-//       var lightState = builder.EntityRecognizer.findEntity(
-//         args.intent.entities,
-//         'Light State'
-//       );
-
-//       // got both location and light state, move on to the next step
-//       if (location && lightState) {
-//         session.dialogData.location = location.entity;
-//         next({
-//           location: location.entity,
-//           lightState: lightState.entity
-//         });
-//       }
-
-//       // got a location, but no light state
-//       if (!location || !lightState) {
-//         session.dialogData.location = location;
-//         builder.Prompts.text(
-//           session,
-//           `I need to know which room and if you want the lights on or off. You can say things like, "Turn on/off the kitchen lights".`
-//         );
-//       }
-//     },
-//     function(session, results) {
-//       var location = results.location;
-//       var lightState = results.lightState;
-//       controlLights(session, location, lightState);
-//     }
-//   ])
-//   .triggerAction({
-//     matches: 'Control Lights'
-//   });
-
-// bot
-//   .dialog('Help', session => {
-//     session.endDialog(
-//       `I can turn lights on or off in any room. You can say things like, "Turn off the lights in the kitchen".`
-//     );
-//   })
-//   .triggerAction({
-//     matches: 'Help'
-//   });
-
-// bot
-//   .dialog('Greeting', session => {
-//     session.endDialog('Sup, yo!');
-//   })
-//   .triggerAction({
-//     matches: 'Greeting'
-//   });
-// https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/160b0864-1ba8-4aee-a337-b8c77695fc9f?subscription-key=19d29a12d3fc4d9084146b466638e62a&verbose=true&timezoneOffset=0&q=
 let luisModelUrl = `https://${process.env['LuisAPIHostName']}/luis/v2.0/apps/${
   process.env['LuisAppId']
 }?subscription-key=${process.env['LuisAPIKey']}`;
@@ -120,16 +54,11 @@ const intents = new builder.IntentDialog({ recognizers: [recognizer] })
 
     // got a location, but no light state
     if (!location || !lightState) {
-      session.dialogData.location = location;
-      builder.Prompts.text(
-        session,
+      session.send(
         `I need to know which room and if you want the lights on or off. You can say things like, "Turn on/off the kitchen lights".`
       );
     }
   })
-  /*
-.matches('<yourIntent>')... See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/
-*/
   .onDefault(session => {
     session.send("Sorry, I did not understand '%s'.", session.message.text);
   });
